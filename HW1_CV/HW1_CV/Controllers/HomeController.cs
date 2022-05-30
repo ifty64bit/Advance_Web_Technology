e23014bc0737b186ab786d61b2515cc3edac0ccf
ifty64bit <19-40447-1@student.aiub.edu>
2022-05-30 20:53:43 +0600

    CV

---
+++ HW1_CV/HW1_CV/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HW1_CV.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+            return View();
+        }
+
+        public ActionResult Address()
+        {
+            return View();
+        }
+    }
+}
---
+++ HW1_CV/HW1_CV/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HW1_CV.Global" Language="C#" %>
